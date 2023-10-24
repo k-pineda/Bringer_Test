588a215fa4302f6c8dfcdd5e01d628acc96a4ff5
k-pineda <karenp9310@gmail.com>
2023-10-24 15:00:51 -0400

    debugged code

--- parcel-tracking-api/index.js
+++ parcel-tracking-api/index.js
@@ -15,18 +15,16 @@ app.post('/Generate_Token', (req, res) => {
   res.json({ token });
 });
 
-// Endpoint to track a parcel
 app.get('/Tracking_Parcel', async (req, res) => {
   const trackingNumber = req.query.Tracking_number;
 
-  // Make a request to the third-party API
   try {
     const response = await axios.get(
       `https://bps.bringer.io/public/api/v2/get/parcel/tracking.json?tracking_number=${trackingNumber}`,
       {
         headers: {
           Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6ImFjY2VzcyJ9.eyJpYXQiOjE2NzIzMjY1NTUsImV4C...',
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6ImFjY2VzcyJ9.eyJpYXQiOjE2NzIzMjY1NTUsImV4cCI6MTcwMzg2MjU1NSwiYXVkIjoiaHR0cHM6Ly9icmluZ2VycGFyY2VsLmNvbSIsImlzcyI6ImZlYXRoZXJzIiwic3ViIjoiNTI1eXM2YWh4d3UyIiwianRpIjoiZDdlZGE3NDgtNzMxOS00YWIzLWI2MGEtMDEzMzI0NmVkNmY2In0.uJi6d6-E2zDWj24wryh2sVWKs4ceny4QllbrHrzK5L0',
         },
       }
     );
@@ -34,6 +32,8 @@ app.get('/Tracking_Parcel', async (req, res) => {
     const parcelInfo = response.data;
     res.json(parcelInfo);
   } catch (error) {
+    console.error('Error:', error.message);
+    console.error('API Response:', error.response.data);
     res.status(500).json({ error: 'Error fetching parcel information' });
   }
 });
